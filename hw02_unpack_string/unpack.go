99b5fe7a7f749be5a83717841729ec7c54f254be
Tetenkov Alexander <forthe@ya.ru>
2022-05-14 12:42:26 +0400

    hw2 done

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -6,7 +6,35 @@ import (
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
+func CheckNumber(num uint8) bool {
+	if num >= 48 && num < 58 {
+		return true
+	}
+	return false
+}
+
+func Unpack(input string) (string, error) {
+	var prevLetter int = -1
+	ret := ""
+	for i := 0; i < len(input); i++ {
+		if CheckNumber(input[i]) {
+			if prevLetter < 0 {
+				return "", ErrInvalidString
+			}
+			if input[i] == 48 {
+				ret = ret[:len(ret)-1]
+			} else {
+				for j := 0; j < int(input[i]-49); j++ {
+					ret += string(uint8(prevLetter))
+				}
+			}
+			prevLetter = -1
+		} else {
+			prevLetter = int(input[i])
+			ret += string(input[i])
+		}
+
+	}
 	// Place your code here.
-	return "", nil
+	return ret, nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,6 +16,10 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		{input: "a1a2a3a4", expected: "aaaaaaaaaa"},
+		{input: "abcdef", expected: "abcdef"},
+		{input: "-.-2-.-", expected: "-.---.-"},
+
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
